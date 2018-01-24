12aba4511d0d4a4047ab36205c38abe2dbe1b5ac
dongshuiquan <1160161078@qq.com>
2018-01-24 20:30:10 +0800

    collection

---
+++ src/main/java/com/collection/ApacheMultiValueMapLinked.java
@@ -0,0 +1,14 @@
+package com.collection;
+
+
+import org.apache.commons.collections.MultiMap;
+import org.apache.commons.collections.map.MultiValueMap;
+
+import java.util.*;
+
+public class ApacheMultiValueMapLinked {
+
+    // Task: parser string with text and show all indexes of all words
+    public static void main(String[] args) {
+    }
+}
